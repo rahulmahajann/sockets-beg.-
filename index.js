4a0edb006d0abb80d0afd928bb6020fc75a29d46
rahul1237 <rahulmahajan98645@gmail.com>
2021-04-25 20:27:07 +0530

    added

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server,{
+    cors:{origin: '*'}
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/',(req,res) => {
+    res.render('home')
+})
+
+server.listen(3001, () => {
+    console.log('server is running... at port 3001');
+})
+
+io.on('connection', (socket) => {
+    console.log('user connected! ' + socket.id);
+})
